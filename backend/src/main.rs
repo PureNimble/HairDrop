cf792f84682ae0936f41fbd478180c5e684369a9
João Pinto <pintojad03@gmail.com>
2024-11-23 12:06:32 +0200

    Add a rate limit requests to 5 per minute per IP

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,3 @@
-extern crate diesel;
-
 mod jwt;
 mod models;
 mod schema;
@@ -7,11 +5,13 @@ mod services;
 mod utils;
 
 use actix_cors::Cors;
+use actix_limitation::Limiter;
 use actix_web::{web, App, HttpServer};
 use diesel::mysql::MysqlConnection;
 use diesel::r2d2::{self, ConnectionManager};
 use lazy_static::lazy_static;
 use std::sync::Arc;
+use std::time::Duration;
 use utils::config::ServerConfig;
 
 use post_login::login_user;
@@ -43,22 +43,24 @@ async fn main() -> std::io::Result<()> {
     // Start the HTTP server
     HttpServer::new(move || {
         let cors = Cors::default()
-            .allow_any_origin()
-            //.allowed_origin("http://hairdrop.me")
-            //.supports_credentials()
+            .allowed_origin("https://hairdrop.me")
             .allow_any_method()
             .allow_any_header()
             .max_age(3600);
 
+        // Rate limit requests to 5 per minute per IP
+        let limiter = Limiter::new().limit_by_ip(5, Duration::from_secs(60));
+
         App::new()
             .wrap(cors) // Attach CORS middleware
+            .wrap(limiter) // Attach rate limiting middleware
             .app_data(web::Data::new(pool.clone()))
             .route("/register", web::post().to(register_user))
             .route("/login", web::post().to(login_user))
             .route("/search", web::post().to(vulnerable_search))
             .route("/searchSafe", web::post().to(secure_search))
     })
-    .bind((SERVER_CONFIG.host.to_string(), SERVER_CONFIG.port))?
+    .bind((SERVER_CONFIG.host.as_str(), SERVER_CONFIG.port))?
     .run()
     .await
 }
